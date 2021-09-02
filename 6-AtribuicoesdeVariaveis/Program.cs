4fb7a06ce4b491e662b05371973e0b73beed779a
Antonio Marcos Barbosa de Sá <antonio.sa@ufu.br>
2021-09-02 10:43:01 -0300

    Add files via upload

---
+++ 6-AtribuicoesdeVariaveis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtribuicoesDeVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 6");
+
+            int idade = 22;
+            int idadeAntonio = idade;
+
+            idade = 20;
+
+            Console.WriteLine(idade);
+            Console.WriteLine(idadeAntonio);
+
+            Console.ReadLine();
+        }
+    }
+}
